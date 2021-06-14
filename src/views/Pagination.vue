d26c4625bade75d815409e3e00f7cd4a36985188
Yaroslav Palaguta <palaguta.y.a@gmail.com>
2021-06-14 23:11:54 +0300

    Initial commit

---
+++ src/views/Pagination.vue
@@ -0,0 +1,38 @@
+
+<template>
+  <div>
+    <button
+        v-for="p in totalPages" :key="p"
+        class="pagination-button button is-large"
+        :class="{active: currentPage === p}"
+        @click="changePage(p)">
+      {{p}}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Pagination",
+  props: ['total', 'items'],
+  data() {
+    return {
+      currentPage: 1
+    }
+  },
+  computed: {
+    totalPages() {
+      return Math.ceil(this.total / 10)
+    }
+  },
+  methods: {
+    changePage(pageNumber) {
+      this.currentPage = pageNumber
+      this.$emit('page-changed', pageNumber)
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
